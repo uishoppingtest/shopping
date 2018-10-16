8870af786fb1c214c7eeec43f5db3fe45d6b75f6
gisttesting <travel.spain.tenerife@gmail.com>
2018-10-16 23:15:36 +0200

    Move browser choice to property file

--- ui-tests/src/main/java/com/shop/driver/WebDriverFactory.java
+++ ui-tests/src/main/java/com/shop/driver/WebDriverFactory.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.ResourceBundle;
+
 import static com.google.common.base.Predicates.isNull;
 import static javaslang.API.*;
 import static javaslang.Predicates.*;
@@ -14,9 +16,10 @@ public class WebDriverFactory {
     public static final String FIREFOX = "firefox";
     public static final String CHROME = "chrome";
 
-    // Make sure that you set "browser" system property during the test run
+    private static final ResourceBundle RB = ResourceBundle.getBundle("config");
+
     public static WebDriver getDriver() {
-        return Match(System.getProperty("browser")).of(
+        return Match(RB.getString("browser")).of(
                 Case(anyOf(isNull(), String::isEmpty), () -> { throw new IllegalStateException("'browser' property is missing!"); }),
                 Case(CHROME::equalsIgnoreCase, () -> new ChromeDriver()),
                 Case(FIREFOX::equalsIgnoreCase, () -> new FirefoxDriver()),
